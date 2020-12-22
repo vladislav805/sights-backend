249ee7b7aad98c548e51ce421eb150de55cdc0d9
Vladislav Veluga <vladislav805@yandex.com>
2020-12-22 03:25:03 +0300

    getUsers возвращает пустой массив, если не передано ни одного уникального userIds (раньше падал с ошибкой SQL)

--- src/utils/users/get-users.ts
+++ src/utils/users/get-users.ts
@@ -4,6 +4,11 @@ import { ICompanion } from '../../handlers/method';
 
 export const getUsers = async(ids: number[], fields: string, companion: ICompanion): Promise<IUser[]> => {
     const uniqueIds = [...new Set(ids)];
+
+    if (!uniqueIds.length) {
+        return [];
+    }
+
     const manager = new UserFieldsManager(fields);
     const { columns, joins } = manager.build(companion.session);
 
