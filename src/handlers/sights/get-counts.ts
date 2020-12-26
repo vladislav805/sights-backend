71642d2352c7b295ab918129c83d935486fe2080
Vladislav Veluga <vladislav805@yandex.com>
2020-12-26 03:54:49 +0300

    Исправление sights.getCounts (verfiied работал неправильно)

--- src/handlers/sights/get-counts.ts
+++ src/handlers/sights/get-counts.ts
@@ -23,7 +23,7 @@ export default class SightsGetCounts extends OpenMethodAPI<never, IResult> {
         const [result] = await props.database.select<IResult>(
             'select \
 (select count(*) from `sight`) as `total`,\
-(select count(*) from `sight` where (`sight`.`mask` & 6) = 6) as `verified`,\
+(select count(*) from `sight` where (`sight`.`mask` & 6) > 0) as `verified`,\
 (select count(*) from `sight` where (`sight`.`mask` & 2) = 2) as `active`,\
 (select count(*) from `sight` where (`sight`.`mask` & 4) = 4) as `archived`\
 ',
